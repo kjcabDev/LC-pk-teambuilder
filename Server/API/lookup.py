439987842d05aba2f8d1e22d8b18ebfe02886cbf
kjcabPL <kcabrera@profoundlogic.com>
2026-09-14 01:22:39 -0400

    - lookup tool search prompt improvements

--- Server/API/lookup.py
+++ Server/API/lookup.py
@@ -7,13 +7,12 @@ def search(name):
     global graph
     target = name.lower().strip()
     search_prompt = f'''
-    Give me a short summary of {target}. Mention the current team lineup in the summary using the current_pokemon_team tool.
-    Your evaluation will start with the base stats of the pokemon as well as 
-    the effort per stat it uses as its growth potential.
+    Give a short summary of {target}. Mention the current team lineup in the summary using the current_pokemon_team tool before proceeding with the rest of the response.
+    Your evaluation will include the base stats of {target} as well as the effort per stat it uses as its growth potential.
     Evaluate {target} with how it ranks in the team lineup from 1 to N, N being the length of the team lineup. Rank 1 as best and N as the least. There can only be at most 6 pokemon in the team at a time.
     Add how {target} makes the team composition weaker or stronger, or if it can specialize with certain abilities to cover weaknesses.
     Provide suggestions for other pokemon or typing in your summary if trying to replace {target} is needed.
-    If {target} is not part of the team, comment if the pokemon is a good or bad addition for the team and who to replace already full.
+    If {target} is not part of the team, comment if it is a good or bad addition for the team and who to replace already full.
     '''
     inputs = {
         'messages': [(
--- Server/Model/utils.py
+++ Server/Model/utils.py
@@ -2,7 +2,7 @@ import requests, json
 from langchain.tools import tool
 
 @tool
-def get_stats(name: str) -> str:
+def get_stats(name: str) -> dict:
     """retrieves the stats from the pokemon API for a given pokemon name"""
     API_URL = f'https://pokeapi.co/api/v2/pokemon/{name.lower().strip()}'
     result = requests.get(API_URL)
@@ -39,18 +39,5 @@ def get_stats(name: str) -> str:
             'is_hidden': ability['is_hidden'],
         })
     pk_data['abilities'] = ab_set
-    result_str = (
-        f'name: {pk_data['name']} | '
-        f'pokedex id: {pk_data['id']} | '
-        f'types: {pk_data['types']} | '
-        f'type 1: {pk_data['type1']} | '
-        f'type 2: {pk_data['type2']} | '
-        f'move count: {pk_data['move_count']} | '
-        f'ability count: {pk_data['ability_count']} | '
-        f'form count: {pk_data['form_count']} | '
-        f'stats: {pk_data['stats']} |'
-        f'abilities: {pk_data['abilities']}'
-    )
 
-    print("Current PK Data: ", result_str)
-    return result_str
+    return pk_data
